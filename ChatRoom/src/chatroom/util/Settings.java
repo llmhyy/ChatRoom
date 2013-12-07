02f6c7a2d3ef9e137073161700abdd5c4cf29837
Lin Yun <llmhyy@gmail.com>
2013-12-07 21:23:30 +0800

    Support configuring network parameters such as listening port, max chatter number into config.properties

--- ChatRoom/src/chatroom/util/Settings.java
+++ ChatRoom/src/chatroom/util/Settings.java
@@ -1,15 +1,40 @@
 package chatroom.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Settings {
 	public static int aliveListenerPort = 8000;
 	public static int messageListenerPort = 8001;
-	public static String[] hosts = {"192.168.1.3"};
-	
-	/*static {
-		hosts = new String[3];
-		for(int i=0; i<3; i++){
-			hosts[i] = "192.168.1." + (i+1); 
+	public static String[] hosts;
+
+	static {
+		Properties prop = new Properties();
+
+		try {
+			// load a properties file
+			prop.load(new FileInputStream("config.properties"));
+
+			// get the property value and print it out
+			String networkSegment = prop.getProperty("network_segment");
+			String chatterNumber = prop.getProperty("chatter_number");
+			String messageListeningPort = prop.getProperty("message_listening_port");
+			String aliveListeningPort = prop.getProperty("active_listening_port");
+			
+			aliveListenerPort = Integer.valueOf(aliveListeningPort);
+			messageListenerPort = Integer.valueOf(messageListeningPort);
+			
+			String networkPrefix = networkSegment.substring(0, networkSegment.indexOf("*"));
+			int count = Integer.valueOf(chatterNumber);
+			hosts = new String[count];
+			for(int i=0; i<count; i++){
+				hosts[i] = networkPrefix + (i+2);
+			}
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
-	}*/
-	
+	}
+
 }
